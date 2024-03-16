3afc087acce244c007534d0c6f9688c231917aa4
Dannick Bedard <dannickbedard@gmail.com>
2024-03-15 23:04:25 -0400

    add neo-tree

--- init.lua
+++ init.lua
@@ -36,6 +36,19 @@ local plugins = {
         indent = { enable = true },
       })
     end
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+      vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
   }
 }
 
