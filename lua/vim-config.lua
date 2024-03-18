1bfe7dc48c4d9b7d1d45454a8b16d67f6845b75e
Dannick Bedard <dannick.bedard@loginnove.ca>
2024-03-18 15:30:52 -0400

    add line color to easy the find via linenumber

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -6,3 +6,9 @@ vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
+
+-- LineNumberColore
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='White', bold=true })
+vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='White', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
