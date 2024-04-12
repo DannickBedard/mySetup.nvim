c021c140e0cee35d0b764bb904d4fa0e38a7ebb1
Dannick Bedard <dannick.bedard@loginnove.ca>
2024-04-12 12:06:25 -0400

    add more vim config

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -14,7 +14,22 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
+vim.opt.wrap = false
+
 vim.g.mapleader = " "
+
+
+opt.updatetime = 50
+
+opt.colorcolumn = "80"
+
+opt.scrolloff = 8 -- Shen scrolling there will always be 8 line on top or bottom
+vim.opt.incsearch = true
+
+vim.opt.swapfile = false
+-- à voir si c'est pertinent -- vim.opt.backup = false
+-- à voir si c'est pertinent --vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- à voir si c'est pertinent --vim.opt.undofile = true
 
 -- LineNumberColore
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
