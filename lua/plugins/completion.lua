e0c4171c613fc56e82699a5e722acc37091c82d1
Dannick Bedard <dannick.bedard@loginnove.ca>
2024-05-23 22:17:22 -0400

    add new snipette for checkbox - [ ]

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -35,9 +35,9 @@ return {
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' }, -- For luasnip users.
-        }, {
-            { name = 'buffer' },
-          })
+          { name = 'path' },
+          { name = 'buffer' },
+        })
       })
 
       -- Set up lspconfig.
