799ba37f4e16ae97191dec0730b6be3bd3de81c0
Dannick Bedard <dannick.bedard@loginnove.ca>
2025-06-28 10:42:01 -0400

    Remove not used code plus new snippet for larave log

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -17,29 +17,29 @@
 return {
   "ellisonleao/gruvbox.nvim", 
   config = function()
-    require("gruvbox").setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = false,
-      italic = {
-        strings = false,
-        emphasis = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    })
+    -- require("gruvbox").setup({
+    --   terminal_colors = true, -- add neovim terminal colors
+    --   undercurl = true,
+    --   underline = true,
+    --   -- bold = false,
+    --   -- italic = {
+    --   --   strings = false,
+    --   --   emphasis = false,
+    --   --   comments = false,
+    --   --   operators = false,
+    --   --   folds = false,
+    --   -- },
+    --   strikethrough = true,
+    --   invert_selection = false,
+    --   invert_signs = false,
+    --   invert_tabline = false,
+    --   inverse = true, -- invert background for search, diffs, statuslines and errors
+    --   contrast = "", -- can be "hard", "soft" or empty string
+    --   palette_overrides = {},
+    --   overrides = {},
+    --   dim_inactive = false,
+    --   transparent_mode = false,
+    -- })
 
     vim.o.background = "dark" -- or "light" for light mode
     vim.cmd([[colorscheme gruvbox]])
